8c2d49da61203c5c306e26fb34faa07002d105e6
netbrum <netbrum@tutanota.com>
2024-12-07 17:29:34 +0100

    optimize day seven solution

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,43 @@
 use std::fs;
 
-fn is_solvable(target: usize, current: usize, values: &[usize]) -> bool {
-    if values.is_empty() {
-        return current == target;
-    }
+fn is_solvable(current: usize, values: &[usize]) -> bool {
+    let value = *values.last().unwrap();
+    let length = values.len();
 
-    let value = values[0];
+    if length == 1 {
+        return value == current;
+    }
 
     // Remove for the part one solution.
-    if is_solvable(
-        target,
-        format!("{current}{value}").parse().unwrap(),
-        &values[1..],
-    ) {
+    //
+    // The mask is 10^(amount of digits). So 6 would be 10, 15 would be 100, etc.
+    //
+    // Example 156: 15 6
+    //
+    // (156 (current) - 6 (value)) % 10 (mask) == 0
+    //
+    // Now we want to truncate the current number to essentially "reverse"
+    // the concatenation, so we divide it by the mask.
+    //
+    // 156 (current) / 10 (mask) = 15 (It's integers, it gets rounded down.)
+    //
+    // The resulting 15 is the first two digits of the current number.
+    //
+    // We then hit the base case of length == 1, with the current number being 15 and the last
+    // number in the values also 15!
+    let mask = 10_usize.pow(value.checked_ilog10().unwrap_or(0) + 1);
+    if current >= value
+        && (current - value) % mask == 0
+        && is_solvable(current / mask, &values[..length - 1])
+    {
         return true;
     }
 
-    if is_solvable(target, current * value, &values[1..]) {
+    if current % value == 0 && is_solvable(current / value, &values[..length - 1]) {
         return true;
     }
 
-    if is_solvable(target, current + value, &values[1..]) {
+    if current >= value && is_solvable(current - value, &values[..length - 1]) {
         return true;
     }
 
@@ -43,7 +60,7 @@ fn main() {
     });
 
     let total: usize = data
-        .filter(|(key, values)| is_solvable(*key, 0, values))
+        .filter(|(key, values)| is_solvable(*key, values))
         .map(|(key, _)| key)
         .sum();
 
