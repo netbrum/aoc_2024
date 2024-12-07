61cd9602b0274d41a484f4e81c70a7db0eed3317
netbrum <netbrum@tutanota.com>
2024-12-07 15:36:55 +0100

    add day seven solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use std::fs;
+
+fn is_solvable(target: usize, current: usize, values: &[usize]) -> bool {
+    if values.is_empty() {
+        return current == target;
+    }
+
+    let value = values[0];
+
+    // Remove for the part one solution.
+    if is_solvable(
+        target,
+        format!("{current}{value}").parse().unwrap(),
+        &values[1..],
+    ) {
+        return true;
+    }
+
+    if is_solvable(target, current * value, &values[1..]) {
+        return true;
+    }
+
+    if is_solvable(target, current + value, &values[1..]) {
+        return true;
+    }
+
+    false
+}
+
 fn main() {
-    println!("Hello, Advent of Code 2024!");
+    let input = fs::read_to_string("input.txt").unwrap();
+    let lines = input.lines();
+
+    let data = lines.map(|line| {
+        let (target, values) = line.split_once(": ").unwrap();
+        let target = target.parse::<usize>().unwrap();
+        let values: Vec<usize> = values
+            .split_whitespace()
+            .map(|v| v.parse::<usize>().unwrap())
+            .collect();
+
+        (target, values)
+    });
+
+    let total: usize = data
+        .filter(|(key, values)| is_solvable(*key, 0, values))
+        .map(|(key, _)| key)
+        .sum();
+
+    println!("{total}");
 }
